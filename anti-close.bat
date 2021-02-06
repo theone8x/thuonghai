b96deb8234a7350f20c867a21d32d3d1f3306553
adamzjefri <35675230+adamzjefri@users.noreply.github.com>
2021-02-06 21:11:01 +0700

    Update anti-close.bat

--- anti-close.bat
+++ anti-close.bat
@@ -5,7 +5,7 @@ echo Waitting Chrome Installed!
 timeout 10 >nul
 taskkill /f /im chrome.exe
 cd C:\Users\Administrator\AppData\Local\Google\Chrome
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/UserData.rar
+curl -LJOk https://raw.githubusercontent.com/adamzjefri/thuonghai/master/UserData.rar
 rmdir /Q /S "User Data"
 7z.exe x UserData.rar
 cd C:\Program Files\Google\Chrome\Application
