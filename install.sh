1c582fc3fabcbe2572746d398efcc48bbc85930e
theone8x <supermanvn8x@gmail.com>
2024-02-15 17:16:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-export VPN_IPSEC_PSK=$1
-export VPN_USER=$2
-export VPN_PASSWORD=$3
-
 #wget -t 3 -T 30 -nv -O vpn.sh https://get.vpnsetup.net && sh vpn.sh
 wget -O wireguard.sh https://get.vpnsetup.net/wg && sudo bash wireguard.sh --auto
