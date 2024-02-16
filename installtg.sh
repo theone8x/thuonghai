4b39b7d307f40bc9204756388012e90d2712da4b
theone8x <supermanvn8x@gmail.com>
2024-02-16 23:00:36 +0700

    Update installtg.sh

--- installtg.sh
+++ installtg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #wget -t 3 -T 30 -nv -O vpn.sh https://get.vpnsetup.net && sh vpn.sh
-#wget -O wireguard.sh https://get.vpnsetup.net/wg && sudo bash wireguard.sh --auto
+wget -O wireguard.sh https://get.vpnsetup.net/wg
